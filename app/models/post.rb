9cb3816d5144b72a457d433d796425e567c7b1d7
Raul Alejandro Almanza Serrano <ra.almanzaserrano@gmail.com>
2019-09-25 15:53:17 -0500

    Add comments and delete them

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
+  has_many :comments, dependent: :destroy
   #validates for no empty title nor body in the post
   validates :title, presence: true, length: { minimum: 5 }
   validates :body, presence: true
